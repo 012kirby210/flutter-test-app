377bfe7b5d0c9c4d8d8d54eb5c1a7a7658f92c98
012kirby210 <012kirby210@gmail.com>
2023-12-27 10:14:55 +0100

    Ajout bouton et modification état global.

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,11 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
@@ -39,7 +44,15 @@ class MyHomePage extends StatelessWidget {
       body: Column(
         children: [
           Text('A random idea.'),
-          Text(appState.current.asLowerCase)
+          Text(appState.current.asLowerCase),
+
+          ElevatedButton(
+            onPressed: () {
+              appState.getNext();
+            },
+            child: Text('Next')
+          ),
+
         ],)
 
     );
