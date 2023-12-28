2f2fe758af6c789c68c818f9d466020489c287cf
012kirby210 <012kirby210@gmail.com>
2023-12-28 07:55:01 +0100

    Ajout liste des mots favoris.

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,16 @@ class MyApp extends StatelessWidget {
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
 
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if ( favorites.contains(current)){
+      favorites.remove(current);
+    }else{
+      favorites.add(current);
+    }
+  }
+
   void getNext() {
     current = WordPair.random();
     notifyListeners();
